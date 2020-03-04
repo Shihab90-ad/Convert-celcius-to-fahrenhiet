849e8438215b6ec21dfb08a1fc48084cb965949a
SADEKATUL SHIHAB <57261325+Shihab90-ad@users.noreply.github.com>
2020-03-04 21:45:13 +0800

    cpp
    this is a c++ program

---
+++ celsius to fahrenhiet.cpp
@@ -0,0 +1,17 @@
+/*@Shihab Sadekatul
+Matric no:1821581
+lab #1 Section #3*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=32;
+	double celsius,fahrenhiet,x=9,y=5;
+	cout<<"Enter a degree in Celsius: ";
+	cin>>celsius;
+	fahrenhiet=(x/y)*celsius+32;
+	cout<<celsius<<" Celsius is "<<fahrenhiet<<" Fahrenhiet"<<endl;
+	system("pause");
+	return 0;
+}
